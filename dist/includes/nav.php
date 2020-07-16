74208be5fbea54f245a9d1451406018fcbf22162
maxwelloniate <maxwelloniate@gmail.com>
2020-07-16 16:53:03 -0400

    Display categories

--- dist/includes/nav.php
+++ dist/includes/nav.php
@@ -1,6 +1,7 @@
  <!-- Navigation -->
  <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
    <div class="container">
+
      <!-- Brand and toggle get grouped for better mobile display -->
      <div class="navbar-header">
        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -11,18 +12,25 @@
        </button>
        <a class="navbar-brand" href="#">Start Bootstrap</a>
      </div>
+
      <!-- Collect the nav links, forms, and other content for toggling -->
      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
        <ul class="nav navbar-nav">
-         <li>
-           <a href="#">About</a>
-         </li>
-         <li>
-           <a href="#">Services</a>
-         </li>
-         <li>
-           <a href="#">Contact</a>
-         </li>
+
+         <?php
+
+          $query = $con->prepare("SELECT * FROM categories");
+          $query->execute();
+          while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+            echo "
+            <li>
+              <a href='#'>{$row['cat_title']}</a>
+            </li>
+            ";
+          }
+
+          ?>
+
        </ul>
      </div>
      <!-- /.navbar-collapse -->
--- dist/includes/sidebar.php
+++ dist/includes/sidebar.php
@@ -3,44 +3,38 @@
   <!-- Blog Search Well -->
   <div class="well">
     <h4>Blog Search</h4>
-    <div class="input-group">
-      <input type="text" class="form-control">
-      <span class="input-group-btn">
-        <button class="btn btn-default" type="button">
-          <span class="glyphicon glyphicon-search"></span>
-        </button>
-      </span>
-    </div>
-    <!-- /.input-group -->
+    <form action="search.php" method="POST">
+      <div class="input-group">
+        <input type="text" name="search" class="form-control">
+        <span class="input-group-btn">
+          <button type="submit" name="submit" class="btn btn-default">
+            <span class="glyphicon glyphicon-search"></span>
+          </button>
+        </span>
+      </div>
+      <!-- /.input-group -->
+    </form> <!-- search form end -->
   </div>
 
   <!-- Blog Categories Well -->
   <div class="well">
     <h4>Blog Categories</h4>
     <div class="row">
-      <div class="col-lg-6">
+      <div class="col-lg-12">
         <ul class="list-unstyled">
-          <li><a href="#">Category Name</a>
-          </li>
-          <li><a href="#">Category Name</a>
-          </li>
-          <li><a href="#">Category Name</a>
-          </li>
-          <li><a href="#">Category Name</a>
-          </li>
-        </ul>
-      </div>
-      <!-- /.col-lg-6 -->
-      <div class="col-lg-6">
-        <ul class="list-unstyled">
-          <li><a href="#">Category Name</a>
-          </li>
-          <li><a href="#">Category Name</a>
-          </li>
-          <li><a href="#">Category Name</a>
-          </li>
-          <li><a href="#">Category Name</a>
-          </li>
+
+          <?php
+          $query = $con->prepare("SELECT * FROM categories");
+          $query->execute();
+          while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+            echo "
+            <li>
+              <a href='#'>{$row['cat_title']}</a>
+            </li>
+            ";
+          }
+          ?>
+
         </ul>
       </div>
       <!-- /.col-lg-6 -->
@@ -49,9 +43,5 @@
   </div>
 
   <!-- Side Widget Well -->
-  <div class="well">
-    <h4>Side Widget Well</h4>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-  </div>
-
+  <?php include('widget.php'); ?>
 </div>
